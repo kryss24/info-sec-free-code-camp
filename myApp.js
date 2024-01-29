4c19c3d93271b0be60b517b421e0729e75ca76c1
kryss24 <113718014+kryss24@users.noreply.github.com>
2024-01-29 20:36:11 +0100

    Update myApp.js

--- myApp.js
+++ myApp.js
@@ -9,7 +9,7 @@ app.use(helmet.ieNoOpen({}));
 var maxAge;
 var ninetyDaysInSeconds = 90*24*60*60;
 app.use(helmet.hsts({maxAge: ninetyDaysInSeconds, force: true}));
-
+app.use(helmet.dnsPrefetchControl({}));
 
 
 
