b27659bda33e7af0ad422891cd658ee54fdfcf9e
kryss24 <113718014+kryss24@users.noreply.github.com>
2024-01-29 20:31:35 +0100

    Update myApp.js

--- myApp.js
+++ myApp.js
@@ -6,7 +6,7 @@ app.use(helmet.frameguard({action: 'deny'}));
 app.use(helmet.xssFilter({}));
 app.use(helmet.noSniff({}));
 app.use(helmet.ieNoOpen({}));
-var maxAgevar;
+var maxAge;
 var ninetyDaysInSeconds = 90*24*60*60;
 app.use(helmet.hsts({maxAge: ninetyDaysInSeconds, force: true}));
 
