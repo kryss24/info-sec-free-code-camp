153167f4769a42cbd8d7c172f01a8195938d3966
kryss24 <113718014+kryss24@users.noreply.github.com>
2024-01-29 21:13:36 +0100

    Update myApp.js

--- myApp.js
+++ myApp.js
@@ -2,7 +2,7 @@ const express = require('express');
 const helmet = require('helmet');
 const app = express();
 app.use(helmet.hidePoweredBy());
-app.use(helmet.frameguard({action: 'deny'}));
+// app.use(helmet.frameguard({action: 'deny'}));
 app.use(helmet.xssFilter({}));
 app.use(helmet.noSniff({}));
 app.use(helmet.ieNoOpen({}));
@@ -11,8 +11,18 @@ var ninetyDaysInSeconds = 90*24*60*60;
 app.use(helmet.hsts({maxAge: ninetyDaysInSeconds, force: true}));
 app.use(helmet.dnsPrefetchControl({}));
 app.use(helmet.noCache({}));
-app.use(helmet.contentSecurityPolicy({directives: {defaultSrc: ["'self'"], scriptSrc: ["'self'",'trusted-cdn.com']}}));
-
+// app.use(helmet.contentSecurityPolicy({directives: {defaultSrc: ["'self'"], scriptSrc: ["'self'",'trusted-cdn.com']}}));
+app.use(helmet({
+  frameguard: {
+    action: 'deny'
+  },
+  contentSecurityPolicy: {
+    directive: {
+      defaultSrc: ["'self'"],
+      scriptionSrc: ["'self'",'trusted-cdn.com'],
+    },
+  },
+}))
 
 
 
